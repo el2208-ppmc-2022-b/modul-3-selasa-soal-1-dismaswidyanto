6ce1a7e9eb6afe4d5fa69d34cce31a1a247f48f2
dismaswidyanto <63232560+dismaswidyanto@users.noreply.github.com>
2022-02-27 21:59:10 +0700

    Update soal-01.c

--- soal-01.c
+++ soal-01.c
@@ -9,43 +9,135 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-/*Format Fungsi sudah disediakan (Jangan Diubah parameter Fungsi)*/
-/*Silahkan tambahkan variable/fungsi baru bila diperlukan*/
 void separate(int uang, int *sejuta, int *limaratus, int *seratus, int *limapuluh, int *duapuluh, int *sepuluh, int *lima, int *dua, int *satu, int *coin)
 {
-	/*Isi dengan program pemisah lembaran uang*/
-	/*Jangan Lupa beri komentar yah :)*/
-	
-	//Print Output
-	printf("%d lembar uang 1.000.000\n", ...);
-	printf("%d lembar uang 500.000\n", ...);
-	printf("%d lembar uang 100.000\n", ...);
-	printf("%d lembar uang 50.000\n", ...);
-	printf("%d lembar uang 20.000\n", ...);
-	printf("%d lembar uang 10.000\n", ...);
-	printf("%d lembar uang 5.000\n", ...);
-	printf("%d lembar uang 2.000\n", ...);
-	printf("%d lembar uang 1.000\n", ...);
-	printf("%d coin\n", ...);
+    //Uang akan dikurangi terus sampai habis
+    while(uang >= 0){
+        if (uang >= 1000000){
+            
+            uang = uang - 1000000;
+            ++(*sejuta);
+        }
+		else if (uang >= 500000){
+            
+            uang = uang - 500000;
+            ++(*limaratus);
+        } 		
+		else if (uang >= 100000){
+            
+            uang = uang - 100000;
+            ++(*seratus);
+        } 
+        else if (uang >= 50000){
+            uang = uang - 50000;
+            ++*limapuluh;
+        }
+        else if (uang >= 20000){
+            uang = uang - 20000;
+            ++*duapuluh;
+        }
+        else if (uang >= 10000){
+            uang = uang - 10000;
+            ++*sepuluh;
+        }
+        else if (uang >= 5000){
+            uang = uang - 5000;
+            ++*lima;
+        }
+        else if (uang >= 2000){
+            uang = uang - 2000;
+            ++*dua;
+        }
+        else if (uang >= 1000){
+            uang = uang - 1000;
+            ++*satu;
+        }
+		else if (uang > 0){
+            uang = uang - 1;
+            ++*coin;
+        }
+        else{
+            break;
+        }
+    }
 
+    //Print Output
+	if (*sejuta != 0){
+        printf("%d lembar uang 1.000.000\n", *sejuta);
+    }
+	if (*limaratus != 0){
+        printf("%d lembar uang 500.000\n", *limaratus);
+    }
+    if (*seratus != 0){
+        printf("%d lembar uang 100.000\n", *seratus);
+    }
+    if (*limapuluh != 0){
+        printf("%d lembar uang 50.000\n", *limapuluh);
+    }
+    if (*duapuluh != 0){
+        printf("%d lembar uang 20.000\n", *duapuluh);
+    }
+    if (*sepuluh != 0){
+        printf("%d lembar uang 10.000\n", *sepuluh);
+    }
+    if (*lima != 0){
+        printf("%d lembar uang 5.000\n", *lima);
+    }
+    if (*dua != 0){
+        printf("%d lembar uang 2.000\n", *dua);
+    }
+    if (*satu != 0){
+        printf("%d lembar uang 1.000\n", *satu);
+    }
+	if (*coin != 0){
+        printf("%d coin\n", *coin);
+    }
 }
 
 int subtract(int uang, int harga){
-	/*Isi dengan program pengurang uang*/
+	uang = uang - harga;
 	return uang;
 }
 
 int main(){
-	/*Tambahkan Variable Input dan Variable Lainya*/
-	/*Realisasi dari program main*/
+    int inputuang;
+	int hargabarang;
+	int kembalian;
+	
+	int sejuta = 0;
+	int limaratus = 0;
+    int seratus = 0;
+    int limapuluh = 0;
+    int duapuluh = 0;
+    int sepuluh = 0;
+    int lima = 0;
+    int dua = 0;
+    int satu = 0;
+	int coin = 0;
+	
+	printf("Program Penghitung Kembalian\n\n");
 	
 	
-	//template output
-	printf("\nKembalian anda adalah :\n");
-	printf("\nUang anda tidak cukup!");
-	printf("\nTidak ada kembalian!");
-
+    //algoritma program utama hanya untuk menerima input uang
+    printf("Masukkan Jumlah Uang : ");
+    scanf("%d", &inputuang);
+	
+	printf("Masukkan Harga Barang : ");
+    scanf("%d", &hargabarang);
+	
+	kembalian = subtract(inputuang, hargabarang);
+	
+	if(kembalian > 0){
+		printf("\nKembalian anda adalah :\n");
+		separate(kembalian, &sejuta, &limaratus, &seratus, &limapuluh, &duapuluh, &sepuluh, &lima, &dua, &satu, &coin);
+	}
+	else if(kembalian < 0){
+		printf("\nUang anda tidak cukup!");
+	}
+	else{
+		printf("\nTidak ada kembalian!");
+	}
 }
 
